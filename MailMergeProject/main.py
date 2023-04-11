b280a76220a5fea60868f66279eca6acbd96a6cc
„christina-eberhard“ <ceberhard@thinkport.digital>
2023-04-11 22:55:11 +0200

    added new Project: MailMergeProject; practicing the usage of absolute & relative filepaths / how to open files and write to them

---
+++ MailMergeProject/main.py
@@ -0,0 +1,14 @@
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        """ replace the placeholder in the starting letter with all the names in the list, but stripping off 
+        all new lines and useless spaces """
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as final_letter:
+            final_letter.write(new_letter)
