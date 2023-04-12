0bc6460d2aafc516ba7e79568cef849db78c7da6
„christina-eberhard“ <ceberhard@thinkport.digital>
2023-04-12 12:39:00 +0200

    finished new Project: US States Game, using the turtle module and pandas library

--- USStatesGame/main.py
+++ USStatesGame/main.py
@@ -10,10 +10,29 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 states = data["state"].to_list()
+guessed_states = []
 
-answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 
-if answer in states:
-    pass
+while len(guessed_states) < 50:
+    answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                              prompt="What's another state's name?").title()
+    if answer == "Exit":
+        missing_states = []
+        for state in states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_file = pandas.DataFrame(missing_states)
+        new_file.to_csv("states_to_learn.csv")
+        break
+    if answer in states:
+        guessed_states.append(answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer)
+
+
 
 
