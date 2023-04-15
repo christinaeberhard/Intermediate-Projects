f0d5b9335fd03c0778ed852bf53283d4f1abbbd1
„christina-eberhard“ <ceberhard@thinkport.digital>
2023-04-15 22:50:51 +0200

    added the try catch exception finally pattern

--- NATOAlphabetProject/main.py
+++ NATOAlphabetProject/main.py
@@ -6,6 +6,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetics = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-result = [phonetics[letter] for letter in word]
-print(result)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        result = [phonetics[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters are supported!")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
